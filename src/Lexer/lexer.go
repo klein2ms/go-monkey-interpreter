a0b5d34fdce8c7715f22e4e5cec4ade2fa74b652
kleinchris01 <44853451+kleinchris01@users.noreply.github.com>
2019-12-16 18:57:44 -0800

    Lexer

---
+++ src/Lexer/lexer.go
@@ -0,0 +1,26 @@
+//lexer/lexer.go
+
+package lexer
+
+type Lexer struct {
+	input 			string
+	position		int  		//current oposition in input (points to current char)
+	readPosition	int			//current reading positon in input (after current char)
+	ch 				byte		//current char under examination
+}
+
+func New(input string) *Lexer {
+	l :=&Lexer{input: input}
+	l.readChar()
+	return l
+}
+
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition += 1
+}
