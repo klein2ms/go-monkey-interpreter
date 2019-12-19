ef43a5f24a2e2f571996fa4a6918511bbfc2721c
Sean Klein <klein2ms@gmail.com>
2019-12-19 00:56:35 +0000

    :sparkles: adding packages to main as an example

--- src/main/server.go
+++ src/main/server.go
@@ -3,22 +3,25 @@
  * Licensed under the MIT License. See LICENSE in the project root for license information.
  *---------------------------------------------------------------------------------------*/
 
- package main
+package main
 
- import (
-	 "fmt"
-	 "io"
-	 "net/http"
- )
- 
- func handle(w http.ResponseWriter, r *http.Request) {	
-	 io.WriteString(w, "Hello, World!")
- }
- 
- func main() {
-	 portNumber := "9000"
-	 http.HandleFunc("/", handle)
-	 fmt.Println("Server listening on port ", portNumber)
-	 http.ListenAndServe(":"+portNumber, nil)
- }
- 
+import (
+	"fmt"
+	"io"
+	"lexer"
+	"net/http"
+	"token"
+)
+
+func handle(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hello, World!")
+}
+
+func main() {
+	portNumber := "9000"
+	http.HandleFunc("/", handle)
+	fmt.Println("Server listening on port ", portNumber)
+	http.ListenAndServe(":"+portNumber, nil)
+	fmt.Println(lexer.New(""))
+	fmt.Println(token.ASSIGN)
+}
