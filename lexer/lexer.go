23294bef0bba799cbf4b00bfad01a1c0c8cda17e
Sean Klein <klein2ms@gmail.com>
2020-01-01 17:17:42 +0000

    :sparkles: extended lexing impl to include key words and identifiers

--- lexer/lexer.go
+++ lexer/lexer.go
@@ -19,19 +19,11 @@ func New(input string) *Lexer {
 	return l
 }
 
-func (l *Lexer) readChar() {
-	if l.readPosition >= len(l.input) {
-		l.ch = 0
-	} else {
-		l.ch = l.input[l.readPosition]
-	}
-	l.position = l.readPosition
-	l.readPosition++
-}
-
 // NextToken consumes the next char from the input for a given Lexer and returns a new Token
 func (l *Lexer) NextToken() token.Token {
 	var tok token.Token
+
+	l.skipWhitespace()
 
 	switch l.ch {
 	case '=':
@@ -53,11 +45,58 @@ func (l *Lexer) NextToken() token.Token {
 	case 0:
 		tok.Literal = ""
 		tok.Type = token.EOF
+	default:
+		if isLetter(l.ch) {
+			tok.Literal = l.read(isLetter)
+			tok.Type = token.LookupIdent(tok.Literal)
+			return tok
+		}
+
+		if isDigit(l.ch) {
+			tok.Literal = l.read(isDigit)
+			tok.Type = token.INT
+			return tok
+		}
+
+		tok = newToken(token.ILLEGAL, l.ch)
 	}
 	l.readChar()
 	return tok
 }
 
-func newToken(tokenType token.Type, ch byte) token.Token { //Helps inititialize tokens
+func (l *Lexer) readChar() {
+	if l.readPosition >= len(l.input) {
+		l.ch = 0
+	} else {
+		l.ch = l.input[l.readPosition]
+	}
+	l.position = l.readPosition
+	l.readPosition++
+}
+
+func (l *Lexer) read(a func(ch byte) bool) string {
+	position := l.position
+	for a(l.ch) {
+		l.readChar()
+	}
+	return l.input[position:l.position]
+}
+
+func (l *Lexer) skipWhitespace() {
+	for l.ch == ' ' || l.ch == '\t' || l.ch == '\n' || l.ch == '\r' {
+		l.readChar()
+	}
+}
+
+// Helps inititialize tokens
+func newToken(tokenType token.Type, ch byte) token.Token {
 	return token.Token{Type: tokenType, Literal: string(ch)}
+}
+
+func isLetter(ch byte) bool {
+	return 'a' <= ch && ch <= 'z' || 'A' <= ch && ch <= 'Z' || ch == '_'
+}
+
+func isDigit(ch byte) bool {
+	return '0' <= ch && ch <= '9'
 }
