2f7b6769422680485abfb1a57684c80fb7380418
Sean Klein <klein2ms@gmail.com>
2020-01-02 21:56:15 +0000

    :sparkles: added the ast for the let statement

---
+++ ast/ast.go
@@ -0,0 +1,67 @@
+package ast
+
+import (
+	"github.com/klein2ms/go-monkey-interpreter/token"
+)
+
+// Node represents a node in the AST
+type Node interface {
+	TokenLiteral() string
+}
+
+// Statement represents a statement in the AST
+type Statement interface {
+	Node
+	statementNode()
+}
+
+// Expression represents an expression in the AST
+type Expression interface {
+	Node
+	expressionNode()
+}
+
+// Program represents a program composed of zero or many Statements
+type Program struct {
+	Statements []Statement
+}
+
+// TokenLiteral returns the TokenLiteral root statement in Program
+func (p *Program) TokenLiteral() string {
+	if len(p.Statements) > 0 {
+		return p.Statements[0].TokenLiteral()
+	}
+
+	return ""
+}
+
+// LetStatement represents a statement for the Let keyword in the AST
+type LetStatement struct {
+	Token token.Token
+	Name  *Identifier
+	Value Expression
+}
+
+func (ls *LetStatement) statementNode() {
+
+}
+
+// TokenLiteral returns the TokenLiteral for a LetStatement
+func (ls *LetStatement) TokenLiteral() string {
+	return ls.Token.Literal
+}
+
+// Identifier represents an identifier in the AST
+type Identifier struct {
+	Token token.Token
+	Value string
+}
+
+func (i *Identifier) expressionNode() {
+
+}
+
+// TokenLiteral returns the TokenLiteral for an Idenfitier
+func (i *Identifier) TokenLiteral() string {
+	return i.Token.Literal
+}
