a7739fe078330324fb9cf878b7b0c982249043ab
Sean Klein <klein2ms@gmail.com>
2020-01-09 02:06:37 +0000

    :sparkles: added impl to parse functions

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,7 @@ package ast
 import (
 	"bytes"
 	"github.com/klein2ms/go-monkey-interpreter/token"
+	"strings"
 )
 
 // Node represents a node in the AST
@@ -298,6 +299,39 @@ func (bs *BlockStatement) String() string {
 	for _, s := range bs.Statements {
 		out.WriteString(s.String())
 	}
+
+	return out.String()
+}
+
+// FunctionLiteral represents a function defintion in the AST
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode() {
+
+}
+
+// TokenLiteral represents the TokenLiteral for the Function
+func (fl *FunctionLiteral) TokenLiteral() string {
+	return fl.Token.Literal
+}
+
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
 
 	return out.String()
 }
--- parser/parser_test.go
+++ parser/parser_test.go
@@ -285,6 +285,78 @@ func TestIfExpression(t *testing.T) {
 	}
 }
 
+func TestFunctionLiteral(t *testing.T) {
+	input := `fn(x, y) { x + y; }`
+
+	program := testStatement(t, input, 1)
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		fatalNotExpressionStatement(t, program)
+	}
+
+	function, ok := stmt.Expression.(*ast.FunctionLiteral)
+	if !ok {
+		fatalNotSpecificExpressionStatement(t, stmt, ast.FunctionLiteral{})
+	}
+
+	if len(function.Parameters) != 2 {
+		t.Fatalf("function literal parameters wrong. want 2, got=%d\n", len(function.Parameters))
+	}
+
+	testLiteralExpression(t, function.Parameters[0], "x")
+	testLiteralExpression(t, function.Parameters[1], "y")
+
+	if len(function.Body.Statements) != 1 {
+		t.Fatalf("function.Body.Statments does not have 1 statment. got=%d\n", len(function.Body.Statements))
+	}
+
+	body, ok := function.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("function body is not ast.ExpressionStatement. got=%T", function.Body.Statements[0])
+	}
+
+	testInfixExpression(t, body.Expression, "x", "+", "y")
+}
+
+func TestFunctionParameterParsing(t *testing.T) {
+	tests := []struct {
+		input          string
+		expectedParams []string
+		statementCount int64
+	}{
+		{"fn() {};", []string{}, 1},
+		{"fn(x) {}", []string{"x"}, 1},
+		{"fn(x, y, z) {}", []string{"x", "y", "z"}, 1},
+	}
+
+	for _, tt := range tests {
+		program := testStatement(t, tt.input, 1)
+
+		stmt := program.Statements[0].(*ast.ExpressionStatement)
+		function := stmt.Expression.(*ast.FunctionLiteral)
+
+		if len(function.Parameters) != len(tt.expectedParams) {
+			t.Errorf(
+				"length parameters wrong. want %d, got=%d\n",
+				len(tt.expectedParams),
+				len(function.Parameters))
+		}
+
+		for i, ident := range tt.expectedParams {
+			testLiteralExpression(t, function.Parameters[i], ident)
+		}
+	}
+}
+
+func fatalNotExpressionStatement(t *testing.T, program *ast.Program) {
+	t.Fatalf("program.Statements[0] is not ast.ExpressionStatement. got=%T", program.Statements[0])
+}
+
+func fatalNotSpecificExpressionStatement(t *testing.T, stmt *ast.ExpressionStatement, expectedType interface{}) {
+	t.Fatalf("exp is not %T. got=%T", expectedType, stmt)
+}
+
 func testStatement(t *testing.T, input string, statementCount int) *ast.Program {
 	l := lexer.New(input)
 	p := New(l)
