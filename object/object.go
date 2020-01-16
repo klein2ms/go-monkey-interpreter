b91a996d44df9679bed20e02454bf56a6809415b
kleinchris01 <44853451+kleinchris01@users.noreply.github.com>
2020-01-15 19:15:03 -0800

    created evaluator_test :fire:
    created test to evaluate evaluator

--- object/object.go
+++ object/object.go
@@ -1,16 +1,49 @@
 import "fmt"
 
-//type method that return its ObjectType
+//defined ObjectType type
 type ObjectType string
 
+//defined constants of INTEGER_OBJ, BOOLEAN_OBJ, and NULL_OBJ
 const (
 	INTEGER_OBJ = "INTEGER"
 )
 
-type Interger struct {
+const (
+	BOOLEAN_OBJ = "BOOLEAN"
+)
+
+const (
+	NULL_OBJ = "NULL"
+)
+
+//defined null, boolean, and integer types
+type Null struct {}
+
+type Boolean struct {
+	Value bool
+}
+
+type Integer struct {
 	Value int64
 }
 
+//passed inspect function for integers
 func (i *Integer) Inspect() string { return fmt.Sprintf("%d, i.Value") }
 
+//passed type function for integers
 func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+//passed inspect function for booleans
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t, b.Value") }
+
+//passed type function for booleans
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+//passed inspect function for null
+func (n *Null) Inspect() string { return "null"}
+
+//passed type function for null
+func (n *Null) Type() ObjectType { return NULL_OBJ }
+
+
+
